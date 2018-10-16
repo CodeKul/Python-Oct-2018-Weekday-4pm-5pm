409fac8c3c8c7e42ccf7fa6af8155aec0fed921d
varunkudalkar <varun.kudalkar@gmail.com>
2018-10-16 17:38:58 +0530

    some more demos

--- ClassesAndObjects.py
+++ ClassesAndObjects.py
@@ -1,7 +1,61 @@
 class MyClass:
-    def __init__(self):
-        print("Object is created!")
     str = 0
+    def __init__(self, str):
+        print("Object is created!")
+        self.str = str
+    
+    def display(self):
+        print(self.str)
 
 
-obj = MyClass()
+# obj = MyClass('Codekul')
+# obj.display()
+
+class Vehicle:
+    
+    def __init__ (self, topSpeed, numberOfWheels):
+        self.topSpeed = topSpeed
+        self.numberOfWheels = numberOfWheels
+
+    def moveForward(self):
+        print('Moving forward...')
+    
+    def moveBackward(self):
+        print('Moving backward...')
+
+    def turnLeft(self):
+        print('Turning left...')
+    
+    def turnRight(self):
+        print('Turning right...')
+
+    def getTopSpeed(self):
+        return self.topSpeed
+
+    def getNumberOfWheels(self):
+        return self.numberOfWheels
+
+    def updateTopSpeed(self, topSpeed):
+        self.topSpeed = topSpeed
+    
+    def updateNumberOfWheels(self, numberOfWheels):
+        self.numberOfWheels = numberOfWheels
+
+v1 = Vehicle(100, 4)
+
+v1.moveForward()
+v1.moveBackward()
+v1.turnLeft()
+v1.turnRight()
+
+print('Number of wheels: {}'.format(v1.getNumberOfWheels()))
+print('Top speed: {}'.format(v1.getTopSpeed()))
+
+v1.updateTopSpeed(200)
+print('Top speed: {}'.format(v1.getTopSpeed()))
+
+v1.topSpeed = 300
+print('Top speed: {}'.format(v1.getTopSpeed()))
+
+
+
---
+++ Student.py
@@ -0,0 +1,83 @@
+class Student:
+    def __init__(self, rno, name, marklist):
+        self.rno = rno
+        self.name = name
+        self.marklist = marklist
+
+    def display(self):
+        print('Roll no.: {}'.format(self.rno))
+        print('Name: {}'.format(self.name))
+        self.marklist.display()
+        
+class MarkList:
+    subject1 = 0
+    subject2 = 0
+    subject3 = 0
+    subject4 = 0
+    subject5 = 0
+
+    def __init__(self, subject1, subject2, subject3, subject4, subject5):
+        self.subject1 = subject1
+        self.subject2 = subject2
+        self.subject3 = subject3
+        self.subject4 = subject4
+        self.subject5 = subject5
+
+    def display(self):
+        print("Subject1: {}".format(self.subject1))
+        print("Subject2: {}".format(self.subject2))
+        print("Subject3: {}".format(self.subject3))
+        print("Subject4: {}".format(self.subject4))
+        print("Subject5: {}".format(self.subject5))
+
+    def totalMarks(self):
+        return self.subject1 + self.subject2 + self.subject3 + self.subject4 + self.subject5
+
+    def percentageMarks(self):
+        return (self.totalMarks()/500.0)*100
+
+class School:
+    name = ''
+    students = []
+
+    def __init__(self, name, students):
+        self.name = name
+        self.students = students
+
+    def display(self):
+        for stu in self.students:
+            stu.display()
+            print('')
+
+    def numberOfStudents(self):
+        return len(self.students)
+
+    def averageMarksOfStudents(self):
+        sum = 0.0
+        for stu in self.students:
+            sum += stu.marklist.percentageMarks()
+        return sum/self.numberOfStudents()
+
+marklist1 = MarkList(90,93,89,85,75)
+stu1 = Student(1, 'Chetan', marklist1)
+
+marklist2 = MarkList(89,69,79,82,61)
+stu2 = Student(2, 'Shweta', marklist2)
+
+marklist3 = MarkList(98,59,76,93,91)
+stu3 = Student(3, 'Amar', marklist3)
+
+marklist4 = MarkList(86,87,76,95,73)
+stu4 = Student(4, 'Aishwarya', marklist4)
+
+marklist5 = MarkList(86,72,91,56,73)
+stu5 = Student(5, 'Akash', marklist5)
+
+students = [stu1, stu2, stu3, stu4, stu5]
+
+school = School('My School',students)
+
+school.display()
+
+print('Average marks: {}'.format(school.averageMarksOfStudents()))
+
